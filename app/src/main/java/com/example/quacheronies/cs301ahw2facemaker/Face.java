e3ee468cd3d6c0d19d43b3f8d5a32e7a35afd893
Michael Quach <Quacheronies@Michaels-MacBook-Air.local>
2018-02-13 15:22:08 -0800

    Initial commit

---
+++ app/src/main/java/com/example/quacheronies/cs301ahw2facemaker/Face.java
@@ -0,0 +1,21 @@
+package com.example.quacheronies.cs301ahw2facemaker;
+
+import android.graphics.Canvas;
+
+/**
+ * Created by Quacheronies on 2/13/18.
+ */
+
+public class Face extends FaceMaker {
+    private int skinColor;
+    private int eyeColor;
+    private int hairColor;
+    private int hairStyle;
+
+    public void randomize() {
+
+    }
+    public void onDraw(Canvas canvas) {
+
+    }
+}
