ca7d39a2592bafed2390c6a731cc31924f6d6bf9
Quacheronies <quach19@Up.edu>
2018-02-15 23:43:43 -0800

    Final Commit

--- app/src/main/java/com/example/quacheronies/cs301ahw2facemaker/Face.java
+++ app/src/main/java/com/example/quacheronies/cs301ahw2facemaker/Face.java
@@ -1,21 +1,194 @@
 package com.example.quacheronies.cs301ahw2facemaker;
 
+import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.view.SurfaceView;
+
+import java.util.Random;
 
 /**
- * Created by Quacheronies on 2/13/18.
+ * This class allows for the drawing of the face on the Surface View of the app
+ *
+ * @author Michael Quach
+ * @created February 13, 2018
+ *
  */
 
-public class Face extends FaceMaker {
-    private int skinColor;
-    private int eyeColor;
-    private int hairColor;
-    private int hairStyle;
+public class Face extends SurfaceView {
+    //set the default color for the skin, eye, and hair to black. Set the hair style to default style at 0.
+    private static int skinColor = Color.BLACK;
+    private static int eyeColor = Color.BLACK;
+    private static int hairColor = Color.BLACK;
+    private static int hairStyle = 0;
+    private Random gen = new Random(); //create a random value generator
 
-    public void randomize() {
+    /**
+     * Constructor for Face class
+     *
+     * @param context
+     */
+    public Face(Context context) { //constructor that calls on randomizer method
+        super(context);
+        general_init();
+    }
+
+    private void general_init()  {
+        randomize(); //initializes the randomize method
+    }
+
+
+    public void setSkinColor(int red, int green, int blue) {
+        skinColor = Color.rgb(red, green, blue); //set the skin color to a specific rgb color
+        invalidate();
+
+        /**
+         External Citation
+         Date: 14 February 2018
+         Problem: Canvas not refreshing whenever I open the app up again
+         Resource:
+         https://stackoverflow.com/questions/10647558/when-its-necessary-to-execute-invalidate-on-a-view
+         Solution: Calling invalidate() calls the OnDraw method whenever the previous drawing needs to be changed
+         */
+    }
+
+    public void setEyeColor(int red, int green, int blue) {
+        eyeColor = Color.rgb(red, green, blue); //set the eye color to a specific rgb color
+        invalidate();
+    }
+
+    public void setHairColor(int red, int green, int blue) {
+        hairColor = Color.rgb(red, green, blue); //set the hair color to a specific rgb color
+        invalidate();
+    }
+
+    public void setHairStyle(int hair) {
+        hairStyle = hair; //change the style of the hair based on an inputted value of 0-2
+        invalidate();
+    }
+
+    public int getHairStyle() { //return and print the new hairstyle
+        return hairStyle;
+    }
+
+    public int[] getColor(int faceColor) { //Retrieves color used whenever current color is changed
+        int currentColor; //current color value
+
+        switch (faceColor) {
+            default: //hair color as default
+                currentColor = hairColor;
+                break;
+            case 0: //eye color
+                currentColor = eyeColor;
+                break;
+            case 1: //skin color
+                currentColor = skinColor;
+                break;
+        }
+
+        return new int[]{getRed(currentColor), getGreen(currentColor), getBlue(currentColor)}; //returns new color based on new rgb values
+    }
+
+    public int getRed(int color) { //allows for the retrival of the red component
+        return color & 0x00ff0000; //initializes red
+    }
+
+    public int getGreen(int color) { //allows for the retrival of the green component
+        return color & 0x0000ff00; //initializes green
+    }
+
+    public int getBlue(int color) { //allows for the retrival of the blue component
+        return color & 0x000000ff; //initializes blue
+    }
+
+    public Paint paint(int color) { //paint method to use different colors
+        Paint newColor = new Paint();
+        newColor.setColor(color);
+        return newColor;
+    }
+
+    public void randomize() { //initializes all the variables to randomly selected valid values
+        //generate a random rgb value with a range of 0 to 255
+        int red = gen.nextInt(255);
+        int green = gen.nextInt(255);
+        int blue = gen.nextInt(255);
+        setSkinColor(red, green, blue); //set this random color to the skin color
+
+        switch (gen.nextInt(3)) { //set the eye color to either a shade of red, blue, or brown
+            case 0: //red eyes
+                red = 200 + gen.nextInt(50);
+                green = 0;
+                blue = 0;
+                break; //end the case
+            case 1: //blue eyes
+                red = 0;
+                green = gen.nextInt(100);
+                blue = 200 + gen.nextInt(50);
+                break;
+            default: //brown eyes (default)
+                red = 50;
+                green = 25;
+                blue = 0;
+                break;
+            /**
+             External Citation
+             Date: 12 February 2018
+             Problem: Needed a way to choose between multiple options
+             Resource:
+             https://developer.android.com/reference/android/widget/Switch.html
+             Solution: Use Switch with cases allows me to set multiple options to choose from
+             */
+        }
+        setEyeColor(red, green, blue); //set the eye color to the chosen value
+
+        setHairColor(red, green, blue); //set the hair color to a random color
+
+        setHairStyle(gen.nextInt(3)); //generate a random hair style between 3 options
+
+        invalidate(); //call the onDraw method and redraw face
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public void onDraw(Canvas canvas) { //draws the face on the Surface View
+        //gather dimensions of the Surface View
+        int width = canvas.getWidth();
+        int height = canvas.getHeight();
+
+
+        switch (hairStyle) { //set the hair style to either of 3 options
+            case 0: //draw a flat top on top of the face
+                canvas.drawRect(Math.round((float) width/3), height/4 - 50, Math.round((float) width/3 * 2), height/4 + 50, paint(hairColor));
+                break;
+            case 1: //draw a bowl cut on top of the face
+                canvas.drawRect(width/2 - 500, height/2, width/2 + 500, height/2 + 500, paint(hairColor));
+                break;
+            case 2: //draw a mini afro on top of the face
+                canvas.drawCircle(width/2 - 500, height/2, 300, paint(hairColor));
+                break;
+        }
+
+        // draw a general head to draw on
+        canvas.drawOval(Math.round((float) width/4), Math.round((float) height/4), Math.round((float) width/4 * 3), height - 75, paint(skinColor));
+
+        //draw a pair of eyes using the drawEye helper method
+        drawEye(canvas, 800, 900, width/2 - 240, width/2 - 120);
+        drawEye(canvas, 800, 900, width/2 + 120, width/2 + 240);
 
     }
-    public void onDraw(Canvas canvas) {
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public void drawEye(Canvas canvas, int top, int bottom, int left, int right) { //helper method the assist in drawing eye
+        int diameter = right - left; //calculate diameter of eye
+
+        //dimension the pupil of eye with size and shift
+        int pupilSize = Math.round(diameter/2);
+        int pupilShift = Math.round(diameter/2 - pupilSize/2);
+
+        canvas.drawOval(left, top, right, bottom, paint(Color.WHITE)); //draw eye
+
+        canvas.drawOval(left + pupilShift, top + pupilShift, right - pupilShift, bottom - pupilShift, paint(Color.BLACK)); //draw pupil of eye
 
     }
 }
--- app/src/main/java/com/example/quacheronies/cs301ahw2facemaker/FaceMaker.java
+++ app/src/main/java/com/example/quacheronies/cs301ahw2facemaker/FaceMaker.java
@@ -1,13 +1,176 @@
 package com.example.quacheronies.cs301ahw2facemaker;
 
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class FaceMaker extends AppCompatActivity {
+/**
+ *
+ * This was tested on a Nexus 9 tablet with Android Studio version 2.3.3
+ * This provides the listeners for the Face class
+ *
+ * @author Michael Quach
+ * @created February 13, 2018
+ *
+ */
+
+public class FaceMaker extends AppCompatActivity implements View.OnClickListener, SeekBar.OnSeekBarChangeListener, AdapterView.OnItemSelectedListener, RadioGroup.OnCheckedChangeListener {
+    Face faceCanvas;
+    SeekBar redSeek, greenSeek, blueSeek;
+    TextView redLabel, greenLabel, blueLabel;
+    Button randomFace;
+    RadioGroup faceRadio;
+    Spinner hairChange;
+
+    /**
+     External Citation
+     Date: 14 February 2018
+     Problem: I was not sure how to call on the Radio Buttons in app
+     Resource:
+     https://stackoverflow.com/questions/8323778/how-to-set-onclicklistener-on-a-radiobutton-in-android
+     Solution: I was suggested to use a Radio Group instead for ease of use and less code
+     */
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) { //initializes and associates widgets with ID's
+        //initializes the game state and layout
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_face_maker);
+
+        //associates widgets with their ID's
+        faceCanvas = (Face) findViewById(R.id.canvas);
+        randomFace = (Button) findViewById(R.id.randomize);
+        redSeek = (SeekBar) findViewById(R.id.redSeekBar);
+        greenSeek = (SeekBar) findViewById(R.id.greenSeekBar);
+        blueSeek = (SeekBar) findViewById(R.id.blueSeekBar);
+        redLabel = (TextView) findViewById(R.id.redText);
+        greenLabel = (TextView) findViewById(R.id.greenText);
+        blueLabel = (TextView) findViewById(R.id.blueText);
+        faceRadio = (RadioGroup) findViewById(R.id.radioOptions);
+        hairChange = (Spinner) findViewById(R.id.hairSelect);
+
+        //link each widget to the event handler class
+        randomFace.setOnClickListener(this);
+        redSeek.setOnSeekBarChangeListener(this);
+        greenSeek.setOnSeekBarChangeListener(this);
+        blueSeek.setOnSeekBarChangeListener(this);
+        faceRadio.setOnCheckedChangeListener(this);
+        hairChange.setOnItemSelectedListener(this);
+        
+        //update buttons and seekbar with new change
+        updateFace();
+
+    }
+
+    private void updateFace() {
+        hairChange.setSelection(faceCanvas.getHairStyle()); //allows spinner to display list of options
+
+        int currentColor[] = {0, 0, 0}; // initializes current color array to nothing
+
+        switch (faceRadio.getCheckedRadioButtonId()) { //updates face based on radio buttons
+            case R.id.hairButton:
+                currentColor = faceCanvas.getColor(0);
+                break;
+            case R.id.eyeButton:
+                currentColor = faceCanvas.getColor(1);
+                break;
+            case R.id.skinButton:
+                currentColor = faceCanvas.getColor(2);
+                break;
+        }
+
+        //updates seekbars and text with progress
+        redSeek.setProgress(currentColor[0]);
+        redLabel.setText("" + redSeek.getProgress());
+        greenSeek.setProgress(currentColor[1]);
+        greenLabel.setText("" + greenSeek.getProgress());
+        blueSeek.setProgress(currentColor[2]);
+        blueLabel.setText("" + blueSeek.getProgress());
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) { //randomizes face in surface view after button pressed
+            case R.id.randomize: //randomize face button
+            randomFace.setClickable(false); //button is unclickable while randomizing
+
+            faceCanvas.randomize(); //randomizes face
+
+            updateFace(); //update all functions in app
+            randomFace.setClickable(true); //allow for a random face to be produced after random face button is pressed
+            break;
+        }
+
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        //adjust the seekbars based on progress change or radio buttons
+        switch (faceRadio.getCheckedRadioButtonId()) {
+            case R.id.hairButton:
+                faceCanvas.setSkinColor(redSeek.getProgress(), greenSeek.getProgress(), blueSeek.getProgress());
+                break;
+            case R.id.eyeButton:
+                faceCanvas.setEyeColor(redSeek.getProgress(), greenSeek.getProgress(), blueSeek.getProgress());
+                break;
+            case R.id.skinButton:
+                faceCanvas.setHairColor(redSeek.getProgress(), greenSeek.getProgress(), blueSeek.getProgress());
+                break;
+        }
+
+
+        switch (seekBar.getId()) {
+            case R.id.redSeekBar:
+                redLabel.setText("" + progress);
+                break;
+            case R.id.greenSeekBar:
+                greenLabel.setText("" + progress);
+                break;
+            case R.id.blueSeekBar:
+                blueLabel.setText("" + progress);
+                break;
+        }
+
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) { //touch tracking not important
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) { //touch tracking not important
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) { //pointer capture not important
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) { //allows the hair to change based on spinner selection
+        faceCanvas.setHairStyle(position);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) { //do nothing when nothing is selected
+
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) { //update the face based on the radio button selection
+        if (group.getId() == R.id.radioOptions) {
+            this.updateFace();
+        }
     }
 }
